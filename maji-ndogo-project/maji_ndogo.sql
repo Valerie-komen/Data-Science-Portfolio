d4324da2fd39edf3a4e19d4b9cd5d94aacea81bc
Valerie-komen <valeriekomen@gmail.com>
2026-01-29 13:01:19 +0300

    Add SQL script for water access analysis
    This SQL script analyzes access to basic water services across provinces and towns in Maji Ndogo, identifying gaps and priorities. It includes data exploration, visit frequency analysis, and average queue time calculations.

---
+++ maji-ndogo-project/maji_ndogo.sql
@@ -0,0 +1,127 @@
+-- ============================================
+-- Project: Maji Ndogo Water Access Analysis
+-- Author: Valerie Komen
+-- Description:
+-- This SQL script analyzes access to basic water servicess across provinces and towns in Maji Ndogo to identify gaps and priorities.
+-- =============================================
+
+/* 1. DATA DESCRIPTION
+Tables used:
+- data_dictionary
+- employee
+- auditor_report
+- global_water_access
+- location
+- project_progress
+- visits
+- water_quality
+- water_source
+- well_pollution
+
+/* DATA EXPLORATION
+-- Purpose: Understand the structure and contents of the database
+
+SELECT * FROM emloyee LIMIT 10;
+SELECT * FROM auditor_report LIMIT 10;
+SELECT * FROM location LIMIT 10;
+SELECT * FROM project_progress LIMIT 10;
+SELECT * FROM water_source LIMIT 10;
+SELECT * FROM visits LIMIT 10;
+SELECT * FROM water_quality LIMIT 10;
+SELECT * FROM well_pollution LIMIT 10;
+
+/* TYPES OF WATER SOURCES 
+-- Purpose: Identify all unique water source types in Maji Ndogo
+
+SELECT DISTINCT type_of_water_source
+FROM water_source;
+
+/* 3. VISIT FREQUENCY ANALYSIS
+-- Purpose: Identify the most visited water sources in Maji Ndogo
+
+SELECT visits.time_in_queue, water_source.type_of_water_source 
+FROM md_water_services.visits
+JOIN water_source
+	ON water_source.source_id = visits.source_id
+WHERE time_in_queue > 500
+ORDER BY time_in_queue DESC
+LIMIT 10;
+
+-- Question 1: How long was the survey?
+SELECT MIN(time_of_record) AS MIN,
+		   MAX(time_of_record) AS MAX,
+       DATEDIFF(MAX(time_of_record),MIN(time_of_record)) AS NO_OF_DAYS
+ FROM visits;
+
+-- Question 2; how long do people queue on average?
+SELECT AVG(IF(time_in_queue = 0, NULL, time_in_queue)) AS modified_time_in_queue
+FROM visits;
+
+-- Question 3; Average queue time on different days
+SELECT dayname(time_of_record) AS Day_of_the_week,
+       ROUND(AVG(IF(time_in_queue = 0, NULL, time_in_queue)), 0) AS avg_queue_time
+FROM visits
+GROUP BY dayname(time_of_record);
+
+-- Question 4; time during the day people collect water
+SELECT time_format(TIME(time_of_record),'%H:00') AS HOUR_of_the_DAY,
+       ROUND(AVG(IF(time_in_queue = 0, NULL, time_in_queue)), 0) AS queue_time
+FROM visits
+GROUP BY time_format(TIME(time_of_record),'%H:00')
+ORDER BY time_format(TIME(time_of_record),'%H:00');
+
+SELECT TIME_FORMAT(TIME(time_of_record), '%H:00') AS hour_of_day,
+-- Sunday
+	ROUND(AVG(
+		CASE
+		WHEN DAYNAME(time_of_record) = 'Sunday' THEN time_in_queue
+		ELSE NULL
+	END
+		),0) AS Sunday,
+-- Monday
+	ROUND(AVG(
+		CASE
+		WHEN DAYNAME(time_of_record) = 'Monday' THEN time_in_queue
+		ELSE NULL
+	END
+		),0) AS Monday,
+-- Tuesday
+ROUND(AVG(
+		CASE
+		WHEN DAYNAME(time_of_record) = 'Tuesday' THEN time_in_queue
+		ELSE NULL
+	END
+		),0) AS Tuesday,
+-- Wednesday
+ROUND(AVG(
+		CASE
+		WHEN DAYNAME(time_of_record) = 'Wednesday' THEN time_in_queue
+		ELSE NULL
+	END
+		),0) AS Wednesday,
+-- Thursday
+ROUND(AVG(
+		CASE
+		WHEN DAYNAME(time_of_record) = 'Thursday' THEN time_in_queue
+		ELSE NULL
+	END
+		),0) AS Thursday,
+-- Friday
+ROUND(AVG(
+		CASE
+		WHEN DAYNAME(time_of_record) = 'Friday' THEN time_in_queue
+		ELSE NULL
+	END
+		),0) AS Friday,
+-- Saturday
+ROUND(AVG(
+		CASE
+		WHEN DAYNAME(time_of_record) = 'Saturday' THEN time_in_queue
+		ELSE NULL
+	END
+		),0) AS Saturday
+FROM visits
+WHERE time_in_queue != 0 -- this excludes other sources with 0 queue times
+GROUP BY time_format(TIME(time_of_record),'%H:00')
+ORDER BY time_format(TIME(time_of_record),'%H:00');
+
